89e1b36b6da950e25d42d3b6893f8d46ec3435ec
Trevor Bernard <trevor.bernard@userevents.com>
2013-10-05 11:21:12 -0300

    Add the ability to load shared lib from resources

---
+++ src/main/java/org/zeromq/jni/Loader.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright 2013 Trevor Bernard
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.zeromq.jni;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class Loader {
+
+    private Loader() {
+    }
+
+    public static void loadLibrary(String library) {
+        try {
+            System.load(bundled(library));
+        } catch (Exception e) {
+            System.loadLibrary(library);
+        }
+    }
+
+    private static String libraryPath(String library) {
+        String osArch = System.getProperty("os.arch");
+        String osName = System.getProperty("os.name");
+        String libraryName = System.mapLibraryName(library);
+        return osName + "-" + osArch + File.pathSeparator + libraryName;
+    }
+
+    private static String bundled(String library) throws IOException {
+        InputStream in = null;
+        OutputStream out = null;
+        try {
+            String libraryName = libraryPath(library);
+            in = Loader.class.getClass().getClassLoader().getResourceAsStream(libraryName);
+            File tmpDir = new File(System.getProperty("java.io.tmpdir"));
+            String s = System.mapLibraryName(library);
+            int idx = s.lastIndexOf('.');
+            File file = null;
+            if (idx >= 0)
+                file = File.createTempFile(s.substring(0, idx), s.substring(idx), tmpDir);
+            else
+                file = File.createTempFile(s, null, tmpDir);
+            file.deleteOnExit();
+            out = new FileOutputStream(file);
+            int cnt;
+            byte buf[] = new byte[4096];
+            while ((cnt = in.read(buf)) >= 1) {
+                out.write(buf, 0, cnt);
+            }
+            return file.getAbsolutePath();
+        } finally {
+            try {
+                in.close();
+            } catch (Exception ignore) {
+            }
+            try {
+                out.close();
+            } catch (Exception ignore) {
+            }
+        }
+    }
+}
--- src/main/java/org/zeromq/jni/ZMQ.java
+++ src/main/java/org/zeromq/jni/ZMQ.java
@@ -20,7 +20,7 @@ import java.nio.ByteBuffer;
 
 public final class ZMQ {
     static {
-        System.loadLibrary("zmqjni");
+        Loader.loadLibrary("zmqjni");
     }
 
     private static native void nativeInit();
@@ -37,39 +37,39 @@ public final class ZMQ {
 
     public static native int zmq_ctx_get(long context, int name);
 
-    public static native boolean zmq_ctx_destroy (long context);
+    public static native boolean zmq_ctx_destroy(long context);
 
-    public static native long zmq_socket (long context, int type);
+    public static native long zmq_socket(long context, int type);
 
-    public static native boolean zmq_bind (long socket, String endpoint);
+    public static native boolean zmq_bind(long socket, String endpoint);
 
-    public static native boolean zmq_unbind (long socket, String endpoint);
+    public static native boolean zmq_unbind(long socket, String endpoint);
 
-    public static native boolean zmq_connect (long socket, String endpoint);
+    public static native boolean zmq_connect(long socket, String endpoint);
 
-    public static native boolean zmq_disconnect (long socket, String endpoint);
+    public static native boolean zmq_disconnect(long socket, String endpoint);
 
-    public static native boolean zmq_close (long socket);
+    public static native boolean zmq_close(long socket);
 
-    public static native int zmq_errno ();
+    public static native int zmq_errno();
 
-    public static native String zmq_strerror (int errnum);
+    public static native String zmq_strerror(int errnum);
 
-    public static native int zmq_send (long socket, byte[] buf, int offset, int len, int flags);
+    public static native int zmq_send(long socket, byte[] buf, int offset, int len, int flags);
 
-    public static native int zmq_send (long socket, ByteBuffer buf, int flags);
+    public static native int zmq_send(long socket, ByteBuffer buf, int flags);
 
-    public static native byte[] zmq_recv (long socket, int flags);
+    public static native byte[] zmq_recv(long socket, int flags);
 
-    public static native int zmq_recv (long socket, byte[] buf, int offset, int len, int flags);
+    public static native int zmq_recv(long socket, byte[] buf, int offset, int len, int flags);
 
-    public static native int zmq_recv (long socket, ByteBuffer buf, int flags);
+    public static native int zmq_recv(long socket, ByteBuffer buf, int flags);
 
-    public static native int zmq_setsockopt (long socket, int option, int value);
+    public static native int zmq_setsockopt(long socket, int option, int value);
 
-    public static native int zmq_setsockopt (long socket, int option, long value);
+    public static native int zmq_setsockopt(long socket, int option, long value);
 
-    public static native int zmq_setsockopt (long socket, int option, byte[] value);
+    public static native int zmq_setsockopt(long socket, int option, byte[] value);
 
     public static native int zmq_poll(long items, int count, long timeout);
 }
