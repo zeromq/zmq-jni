d03c14b7f5aee7f9f75da3b4893ae984d2147325
Trevor Bernard <trevor.bernard@gmail.com>
2014-08-09 17:19:09 -0300

    Add unit test cor zmq_z85_encode

--- src/test/clojure/zeromq/zmq.clj
+++ src/test/clojure/zeromq/zmq.clj
@@ -174,6 +174,9 @@
   (close [this]
     (ZMQ/zmq_close address)))
 
+(defn z85-encode [dest buf]
+  (ZMQ/zmq_z85_encode dest buf))
+
 (defn socket [context socket-type]
   (if-let [type (socket-types socket-type)]
     (->ManagedSocket (ZMQ/zmq_socket (:address context) type))
